c2c94c5b18fef04a9c724c778085c46458f75c89
LavaPower <lavapower84@gmail.com>
2020-04-21 18:31:11 +0200

    Create Platform

---
+++ src/entities/platform.rs
@@ -0,0 +1,26 @@
+use sprite::*;
+use piston_window::*;
+use crate::utils::{sprite::load_sprite, Position};
+
+pub struct Platform {
+    pub pos: Position,
+    pub sprite: Sprite<gfx_texture::Texture<gfx_device_gl::Resources>>
+}
+
+impl Platform {
+    pub fn new(pos: Position, sprite: &str, factory: &mut gfx_device_gl::Factory) -> Platform {
+        let mut sprite = load_sprite(factory, sprite);
+        sprite.set_scale(0.6, 0.6);
+        sprite.set_position(pos.x as f64, pos.y as f64);
+        Platform {
+            pos: pos,
+            sprite: sprite
+        }
+    }
+
+    pub fn render<G>(&mut self, c: piston_window::Context, g: &mut G) 
+        where G: Graphics<Texture=gfx_texture::Texture<gfx_device_gl::Resources>>
+    {
+        self.sprite.draw(c.transform, g);
+    }
+}
--- src/states/game.rs
+++ src/states/game.rs
@@ -1,16 +1,19 @@
 use piston_window::*;
 
 use crate::entities::*;
+use crate::utils::Position;
 use crate::AppInfo;
 
 pub struct Game {
-    pub elsa: Elsa
+    pub elsa: Elsa,
+    pub platforms: Vec<Platform>
 }
 
 impl Game {
     pub fn new(factory: &mut gfx_device_gl::Factory) -> Game {
         Game {
-            elsa: Elsa::new(factory)
+            elsa: Elsa::new(factory),
+            platforms: vec!(Platform::new(Position::new(100, 500), "./resources/images/Tiles/15.png", factory))
         }
     }
 
@@ -23,6 +26,9 @@ impl Game {
 
     pub fn draw<G : Graphics<Texture=gfx_texture::Texture<gfx_device_gl::Resources>>>(&mut self, c: piston_window::Context, g: &mut G, device: &mut gfx_device_gl::Device, glyphs: &mut Glyphs) {
         self.elsa.render(c, g);
+        for i in 0..self.platforms.len() {
+            self.platforms[i].render(c, g);
+        }
     }
     
 }
