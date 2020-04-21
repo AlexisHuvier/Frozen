4a4ad856f692c1e6e335023c5e6ca111ec7f5766
LavaPower <lavapower84@gmail.com>
2020-04-21 16:15:22 +0200

    Rework on Window management

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use piston_window::*;
 use crate::utils::{FPSCounter, Color, Config};
-use crate::states::{Menu, Options};
+use crate::states::*;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum States {
@@ -9,7 +9,6 @@ pub enum States {
 }
 
 pub struct App {
-    win: PistonWindow,
     debug: bool,
     menu: Menu,
     options: Options,
@@ -17,12 +16,10 @@ pub struct App {
 }
 
 impl App {
-    pub fn new() -> App {
-        let size = Size { width: 1280., height: 960. };
+    pub fn new(factory: &mut gfx_device_gl::Factory, size: Size) -> App {
         let conf = Config::new("./resources/config.json");
         let debug = conf.get("debug").as_bool().expect("[Config] Debug value must be boolean");
         App {
-            win: WindowSettings::new("Frozen", size).resizable(false).build().unwrap_or_else(|e| panic!("Failed to build App: {}", e)),
             debug: debug,
             menu: Menu::new(size),
             options: Options::new(size, conf),
@@ -30,12 +27,12 @@ impl App {
         }
     }
 
-    pub fn run(&mut self) {
-        let mut glyphs = self.win.load_font("./resources/fonts/general.ttf").expect("Unable to load font : general.ttf");
+    pub fn run(&mut self, win: &mut PistonWindow) {
+        let mut glyphs = win.load_font("./resources/fonts/general.ttf").expect("Unable to load font : general.ttf");
         let mut fps_counter = FPSCounter::new();
 
-        while let Some(e) = self.win.next() {
             let debug = self.debug;
+        while let Some(e) = win.next() {
 
             match self.state {
                 States::Menu => {
@@ -58,17 +55,13 @@ impl App {
                 }
             }
 
-            let mut menu = self.menu.clone();
-            let mut options = self.options.clone();
-            let state = self.state;
-
-            self.win.draw_2d(&e, |c, g, device| {
+            win.draw_2d(&e, |c, g, device| {
                 let fps = fps_counter.tick();
                 clear(Color::new(0, 197, 255).get_float(), g);
                 
                 match state {
-                    States::Menu => menu.draw(c, g, device, &mut glyphs),
-                    States::Options => options.draw(c, g, device, &mut glyphs)
+                    States::Menu => self.menu.draw(c, g, device, &mut glyphs),
+                    States::Options => self.options.draw(c, g, device, &mut glyphs),
                 }
 
                 if debug {
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ extern crate piston_window;
 mod app;
 mod utils;
 mod states;
+mod entities;
 
 pub use app::*;
+use piston_window::*;
 
 fn main() {
-    let mut app = App::new();
-    app.run();
+    let size = Size { width: 1280., height: 960. };
+    let mut win: PistonWindow = WindowSettings::new("Frozen", size).resizable(false).build().unwrap_or_else(|e| panic!("Failed to build App: {}", e));
+    let mut factory = win.factory.clone();
+    let mut app = App::new(&mut factory, size);
+    app.run(&mut win);
 }
