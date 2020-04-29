a0f527a73d035df9672a6d1c26c3dc53b3657af2
LavaPower <lavapower84@gmail.com>
2020-04-29 15:03:29 +0200

    Make scale parameter of Platform and add set_position

--- src/entities/platform.rs
+++ src/entities/platform.rs
@@ -8,14 +8,18 @@ pub struct Platform {
 }
 
 impl Platform {
-    pub fn new(pos: Position, sprite: &str, factory: &mut gfx_device_gl::Factory) -> Platform {
+    pub fn new(pos: Position, sprite: &str, factory: &mut gfx_device_gl::Factory, scale: f64) -> Platform {
         let mut sprite = load_sprite(factory, sprite);
-        sprite.set_scale(0.59375, 0.59375);
+        sprite.set_scale(scale, scale);
         sprite.set_position(pos.x as f64, pos.y as f64);
         Platform {
             pos: pos,
             sprite: sprite
         }
+    }
+
+    pub fn set_position(&mut self, pos: Position) {
+        self.sprite.set_position(pos.x as f64, pos.y as f64);
     }
 
     pub fn render<G>(&mut self, c: piston_window::Context, g: &mut G) 
